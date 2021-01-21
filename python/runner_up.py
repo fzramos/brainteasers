abb9aaf63b44e6765290a3235e2fd0c361ee48cf
fzramos <64625115+fzramos@users.noreply.github.com>
2021-01-20 23:26:02 -0600

    First python folder and exercise added

---
+++ python/runner_up.py
@@ -0,0 +1,49 @@
+"""
+Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given
+
+scores. Store them in a list and find the score of the runner-up.
+
+Input Format
+
+The first line contains
+. The second line contains an array of
+
+integers each separated by a space.
+
+Constraints
+
+Output Format
+
+Print the runner-up score.
+
+Sample Input 0
+
+5
+2 3 6 6 5
+
+Sample Output 0
+
+5
+
+Explanation 0
+
+Given list is
+. The maximum score is , second maximum is . Hence, we print as the runner-up score. 
+"""
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    # first attempt: Use sets and difference
+    # unique = set(arr)
+    
+    # m = max(unique)
+    # no_max = unique.difference({m})
+    # print(max(no_max))
+    
+    # second attempt: sets and remove
+    unique = set(arr)
+    unique.remove(max(unique))
+    print(max(unique))
+    
